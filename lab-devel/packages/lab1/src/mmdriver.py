9f3bf2681b2acc8f7e9223f88de86bd1e2196311
RyanLitz <ryan.litz04@gmail.com>
2025-09-16 10:19:28 -0400

    attempting to move it in a square

--- lab-devel/packages/lab1/src/mmdriver.py
+++ lab-devel/packages/lab1/src/mmdriver.py
@@ -1,15 +1,67 @@
 #!/usr/bin/env python3
 import rospy
 import os
+import time
 from duckietown_msgs.msg import WheelsCmdStamped # Import the message for the wheel comm
 class Driver():#CHANGE CLASSNAME to the name of your class
     def __init__(self):
         self.veh_name = os.environ['VEHICLE_NAME']
+        self.pub = rospy.Publisher('/ee483mm07/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=10) #our publisher
         # USING PARAMETER TO GET THE NAME OF THE VEHICLE
         # THIS WILL BE USEFUL TO SPECIFY THE NAME OF THE TOPIC
         # INITIALIZE YOUR VARIABLES HERE (SUBSCRIBERS OR PUBLISHERS)
-    def drive(self): # CHANGE TO THE NAME OF YOUR FUNCTION
-        print("Driving the MM " + self.veh_name + " around the block") # Just for testin
+    def turn90(self):
+        #commands to turn 90 degrees
+        cmd_to_publish = WheelsCmdStamped()
+        cmd_to_publish.header.stamp = rospy.Time.now()
+
+        cmd_to_publish.vel_right = 0.2
+        cmd_to_publish.vel_left = -0.2
+        self.pub.publish(cmd_to_publish)
+
+        time.sleep(1.15)
+
+        cmd_to_publish.vel_right = 0
+        cmd_to_publish.vel_left = 0
+        self.pub.publish(cmd_to_publish)
+
+    def moveForward(self):
+        cmd_to_publish = WheelsCmdStamped()
+        cmd_to_publish.header.stamp = rospy.Time.now()
+
+        cmd_to_publish.vel_right = 0.5
+        cmd_to_publish.vel_left = 0.5
+
+        self.pub.publish(cmd_to_publish)
+        
+        #print("Driving the MM " + self.veh_name + " around the block") # Just for testin
+        
+        time.sleep(3.5)
+        cmd_to_publish.vel_right = 0
+        cmd_to_publish.vel_left = 0
+
+        self.pub.publish(cmd_to_publish)
+
+
+    def square(self): # CHANGE TO THE NAME OF YOUR FUNCTION
+        cmd_to_publish = WheelsCmdStamped()
+        cmd_to_publish.header.stamp = rospy.Time.now()
+
+        time.sleep(1)
+
+        for i in range(4):
+            self.pub.publish(cmd_to_publish)
+            time.sleep(0.5)
+            drive.moveForward()
+            self.pub.publish(cmd_to_publish)
+            time.sleep(0.5)
+            drive.turn90()
+            self.pub.publish(cmd_to_publish)
+        
+        self.pub.publish(cmd_to_publish)
+        rospy.signal_shutdown("complete")
+        self.pub.publish(cmd_to_publish)
+
         #WRITE THE CODE TO MAKE THE MM GO AROUND THE BLOCK
 if __name__ == "__main__": ## The main function which will be called when your python sc
 # Initialize the node
@@ -20,6 +72,6 @@ if __name__ == "__main__": ## The main function which will be called when your p
         # Keep the line above - you might be able to reduce the delay a bit,
         while not rospy.is_shutdown(): # Run ros forever - you can change
             # this as well instead of running forever
-            drive.drive() # calling your node function
+            drive.square() # calling your node function
     except rospy.ROSInterruptException:
         pass
